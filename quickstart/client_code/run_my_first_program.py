df9f1933a83954111d2bd15ec4ee98b62e8f8957
kavish-100403 <89000018+kavish-100403@users.noreply.github.com>
2024-07-15 22:57:21 +0530

    Update run_my_first_program.py

--- quickstart/client_code/run_my_first_program.py
+++ quickstart/client_code/run_my_first_program.py
@@ -1,11 +1,11 @@
 from nada_dsl import *
 
 def nada_main():
-    party2 = Party(name="Party2")
+    party1 = Party(name="Party1")
 
-    secret_num1 = SecretInteger(Input(name="secret_num1", party=party2))
-    secret_num2 = SecretInteger(Input(name="secret_num2", party=party2))
+    secret_num1 = SecretInteger(Input(name="secret_num1", party=party1))
+    secret_num2 = SecretInteger(Input(name="secret_num2", party=party1))
 
-    sum_result = secret_num1 + secret_num2
+    comparison_result = secret_num1 > secret_num2
 
-    return [Output(sum_result, "sum_output", party2)]
+    return [Output(comparison_result, "comparison_output", party=party1)]
